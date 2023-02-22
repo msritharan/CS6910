ef5e4a8e516336ccd8b2d9a47b51633aa1f15ac3
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-02-22 12:03:40 +0530

    Added Backprop

--- Assignment 1/neural_network.py
+++ Assignment 1/neural_network.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 class FeedforwardNN:
     def __init__(self, weight_init, num_layers, hidden_size, activation, input_size, output_size):
@@ -11,14 +10,21 @@ class FeedforwardNN:
         
         # weights between layers
         self.weights = []
-        self.weights.append(np.zeros(input_size, hidden_size))
+        self.weights.append(np.zeros(hidden_size, input_size))
         layer_id = 1
         while(layer_id < num_layers):
             self.weights.append(np.zeros(hidden_size, hidden_size))
-        self.weights.append(np.zeros(hidden_size, output_size))
+        self.weights.append(np.zeros(output_size, hidden_size))
 
-        # initialize weights as per 'weight_init'
+        # bias for each layers
+        self.bias = []
+        for L in range(num_layers):
+            self.bias.append(np.zeros(hidden_size))
+        self.bias.append(np.zeros(output_size))
+
+        # initialize weights nad bias as per 'weight_init'
         # uniform random and normalized xavier
+        # bias is initialized to zeros 
         if(weight_init == "random"):
             for idx in range(len(self.weights)):
                 self.weights[idx] = np.random.uniform(low = -0.2, high = 0.2, size = self.weights[idx].shape)
@@ -27,14 +33,11 @@ class FeedforwardNN:
                 d0, d1 = self.weights[idx].shape
                 self.weights[idx] = np.sqrt(1/d0)*np.random.randn(d0, d1)
 
-        # bias of layers
-        # can bias be initialized with zeros (?)
-        self.bias = np.zeros(num_layers + 1)
 
     def activate_layer(self, a):
         if self.activation == "identity":
             return self.identity(a)
-        if self.activation == "sigmoid":
+        elif self.activation == "sigmoid":
             return self.sigmoid(a)
         elif self.activation == "tanh":
             return self.tanh(a)
@@ -73,26 +76,62 @@ class FeedforwardNN:
     def grad_softmax(self, x):
         return np.diag(x) - np.outer(x, x)
 
+    def grad_activate(self, x):
+        if self.activation == "identity":
+            return self.grad_identity(x)
+        elif self.activation == "sigmoid":
+            return self.grad_sigmoid(x)
+        elif self.activation == "tanh":
+            return self.grad_tanh(x)
+        elif self.activation == "ReLU":
+            return self.grad_ReLU(x)
+        
     def forward_propagate(self, input):
         h = input
         a_layer = [] # pre activation outputs of each layer
         h_layer = [] # activated outputs of each layer
-        # computes h till the last hidden layer
-        for idx in range(len(self.weights) - 1):
-            # compute pre activated output 'a'
-            a = np.matmul(self.weights[idx].T, h)
+        
+        for l in range(len(self.weights)):
+            a = self.bias[l] + self.weights[l]*h
             h = self.activate_layer(a)
             a_layer.append(a)
             h_layer.append(h)
         
-        # output layer computation
-        idx = len(self.weights) - 1
-        a = np.matmul(self.weights[idx], h)
-        h = self.softmax(a)
+        L = len(self.weights) - 1
+        a = self.bias[L] + self.weights[L]*h
+        h = self.softmax(a) 
         a_layer.append(a)
-        h_layer.append(h)
+        h_layer.append(h) 
 
         return a_layer, h_layer, h
     
+    def back_propagation(self, a_layer, h_layer, ypred, ylabel):
+        grad_W = [] #   list of gradients wrt weights between layers
+        grad_b = [] #   list of gradients wrt biases
+        L = len(h_layer) - 1
+
+        # gradient wrt to a_L
+        e_y = np.zeros(self.output_size)
+        e_y[ylabel] = 1
+
+        # propagate backwards
+        g_a = -(e_y - ypred)
+        for l in range(L, -1, -1):
+            # gradients wrt weights and bias
+            g_Wl = np.outer(g_a, h_layer[l])
+            g_bl = g_a
+            grad_W.append(g_Wl)
+            grad_b.append(g_bl)
+
+            #  gradients wrt layer below
+            g_h = self.weights[l].T*g_a
+            g_a = g_h*self.grad_activate(a_layer[l])
+
+        grad_W.reverse()
+        grad_b.reverse()
+
+        return grad_W, grad_b
+
+
 
 
