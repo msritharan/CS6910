363c0e237cea4404e08c176cff4361c7ae7cde9b
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-03-02 21:22:35 +0530

    fixed small bug

--- Assignment 1/hyperparameter_tuning.py
+++ Assignment 1/hyperparameter_tuning.py
@@ -38,7 +38,7 @@ sweep_configuration = {
         'hidden_layer_size': { "values" : [32, 64, 128] },
         'weight_decay': { "values" : [0, 0.0005, 0.5] },
         'learning_rate': { "values" : [1e-3, 1e-4] },
-        'optimizer' : { "values" : ["sgd", "momentum", "nesterov", "rmsprop", "adam", "nadam"] },
+        'optimizer' : { "values" : ["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"] },
         'batch_size' : { "values" : [16, 32, 64] },
         'weight_init' : { "values" : ["random", "Xavier"] },
         'activation' : { "values" : ["sigmoid", "tanh", "ReLU"] },
