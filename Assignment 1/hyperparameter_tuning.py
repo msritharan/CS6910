463438f99909229831fd9e6d07c7c3cda4105cde
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-03-03 15:39:03 +0530

    changed parameter names
    for consistency with get_inputs.py

--- Assignment 1/hyperparameter_tuning.py
+++ Assignment 1/hyperparameter_tuning.py
@@ -34,15 +34,15 @@ sweep_configuration = {
     'metric': {'name' : 'val_loss', 'goal' : 'minimize'},
     'parameters' : {
         'epochs': { "values" : [5, 10] },
-        'num_hidden_layers': { "values" : [3, 4, 5] },
-        'hidden_layer_size': { "values" : [32, 64, 128] },
+        'num_layers': { "values" : [3, 4, 5] },
+        'hidden_size': { "values" : [32, 64, 128] },
         'weight_decay': { "values" : [0, 0.0005, 0.5] },
         'learning_rate': { "values" : [1e-3, 1e-4] },
         'optimizer' : { "values" : ["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"] },
         'batch_size' : { "values" : [16, 32, 64] },
         'weight_init' : { "values" : ["random", "Xavier"] },
         'activation' : { "values" : ["sigmoid", "tanh", "ReLU"] },
-        'loss_function' : { "values" : ["mean_squared_error", "cross_entropy"] }
+        'loss' : { "values" : ["mean_squared_error", "cross_entropy"] }
     }
 }
 
