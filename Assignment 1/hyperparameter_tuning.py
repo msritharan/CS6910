054568d3c2f523d1c70bd70fe8c8839ed40de229
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-03-19 22:54:37 +0530

    small change

--- Assignment 1/hyperparameter_tuning.py
+++ Assignment 1/hyperparameter_tuning.py
@@ -3,11 +3,15 @@ from optimizers import *
 from get_inputs import *
 import numpy as np
 from keras.datasets import fashion_mnist
+from keras.datasets import mnist
 import wandb
 wandb.login()
 
 # Load Dataset
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+if dataset == "fashion_mnist":
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+elif dataset == "mnist":
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], -1)/255
 x_test = x_test.reshape(x_test.shape[0], -1)/255
 
